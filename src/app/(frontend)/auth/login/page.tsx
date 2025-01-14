d220be5f09d5bb45e69873715e182f237a5caf47
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2025-01-15 00:00:30 +0100

    style(ui): update texts with appropriate gray colour variants

--- src/app/(frontend)/auth/login/page.tsx
+++ src/app/(frontend)/auth/login/page.tsx
@@ -7,18 +7,19 @@ export default function Page() {
     <div className="px-4 text-white pt-24 tracking-[0.37px]">
       <div className=" text-center">
         <h2 className="text-xl leading-[25.78px] font-medium mb-3">Login as a Siwes Applicate</h2>
-        <div className="text-[12px] leading-[16.5px] mb-8">
-          Enter your username, and we’ll send a password reset link to the associated email address.
+        <div className="text-[12px] text-gray-light02 leading-[16.5px] mb-8">
+          Enter your username, and we&apos;ll send a password reset link to the associated email
+          address.
         </div>
       </div>
 
       <Label>Username</Label>
       <Input
         placeholder="Matric Number"
-        className="bg-white/40 backdrop-blur-[70px] placeholder:text-[#F1F1F1]"
+        className="bg-white/40 backdrop-blur-[70px] placeholder:text-gray-light05"
       />
 
-      <Button size="lg" className="w-full mt-8">
+      <Button size="lg" className="w-full mt-8 text-gr">
         Confirm
       </Button>
     </div>
