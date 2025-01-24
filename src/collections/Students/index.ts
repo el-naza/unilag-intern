da4e95a085145dd4cf4afec9a7f7ba779907eb46
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2025-01-24 19:14:56 +0100

    feat: add otp for password reset for students

--- src/collections/Students/index.ts
+++ src/collections/Students/index.ts
@@ -5,6 +5,9 @@ import { self } from '@/access/self'
 import { anyone } from '@/access/anyone'
 import { NextResponse as Response } from 'next/server'
 import { z } from 'zod'
+import * as otpGenerator from 'otp-generator'
+import bcrypt from 'bcryptjs'
+import { isBefore } from 'date-fns'
 
 const PreLogin = z.object({
   matricNo: z.string(),
@@ -23,17 +26,38 @@ export const Students: CollectionConfig = {
     read: authenticatedUsers,
     update: self,
   },
-  auth: {
-    loginWithUsername: true,
-  },
   hooks: {
     beforeOperation: [
       async ({ args, req }) => {
         // in order to login with matric no. as the username thru REST API
-        if (req.data?.matricNo) args.data.username = req.data?.matricNo
+        if (req.data?.matricNo && args.data) args.data.username = req.data?.matricNo
+
         return args
       },
     ],
+  },
+  auth: {
+    loginWithUsername: true,
+    forgotPassword: {
+      generateEmailHTML: (args) => {
+        console.log('in gen, otp frm ctx', args?.req?.context)
+        const resetPasswordURL = `https://yourfrontend.com/reset-password?token=${args?.token}`
+        return `
+          <!doctype html>
+          <html>
+            <body>
+              <h1>Here is my custom email template!</h1>
+              <p>Hello, ${args?.user.email}!</p>
+              <p>Click below to reset your password.</p>
+              <p>
+                <a href="${resetPasswordURL}">${resetPasswordURL}</a>
+                <a href="${resetPasswordURL}">${resetPasswordURL}</a>
+              </p>
+            </body>
+          </html>
+        `
+      },
+    },
   },
   endpoints: [
     {
@@ -65,6 +89,118 @@ export const Students: CollectionConfig = {
         return Response.json({
           message: `User has ${hasSetPassword ? '' : 'not'} set password`,
           ready: !!hasSetPassword,
+        })
+      },
+    },
+    {
+      method: 'post',
+      path: '/send-password-reset-otp',
+      handler: async (req) => {
+        const { data } = PreLogin.safeParse(await req.json?.())
+
+        if (!data) {
+          return Response.json({ message: 'Matric no. not specified' }, { status: 400 })
+        }
+
+        // gen otp and add to context for use in the html gen
+        req.context.otp = otpGenerator.generate(
+          (process.env.OTP_LENGTH as unknown as number) || 6,
+          {
+            upperCaseAlphabets: false,
+            lowerCaseAlphabets: false,
+            specialChars: false,
+          },
+        ) as string
+
+        let res = await req.payload.forgotPassword({
+          collection: 'students',
+          data: {
+            username: data!.matricNo,
+          } as any,
+          req: req,
+        })
+
+        if (!res) {
+          return Response.json(
+            { message: 'This matric no. has not been registered yet' },
+            { status: 404 },
+          )
+        }
+
+        req.payload.update({
+          collection: 'students',
+          where: {
+            matricNo: {
+              equals: data!.matricNo,
+            },
+          },
+          data: {
+            resetPasswordOtpHash: await bcrypt.hash(
+              req.context.otp as string,
+              Number(process.env.BCRYPT_SALT) || 4,
+            ),
+          } as any,
+          req: req,
+        })
+
+        return Response.json({
+          message: 'Success',
+        })
+      },
+    },
+    {
+      method: 'post',
+      path: '/verify-password-reset-otp',
+      handler: async (req) => {
+        const { matricNo, otp } = await req.json?.()
+
+        if (!matricNo || !otp) {
+          return Response.json({ message: 'Matric no. and otp must be specified' }, { status: 400 })
+        }
+
+        const studentFindRes = await req.payload.find({
+          collection: 'students',
+          where: {
+            matricNo: { equals: matricNo },
+          },
+          showHiddenFields: true,
+        })
+
+        if (studentFindRes.docs.length === 0) {
+          return Response.json(
+            { message: 'This matric no. has not been registered yet' },
+            { status: 404 },
+          )
+        }
+
+        const { resetPasswordOtpHash, resetPasswordToken, resetPasswordExpiration, id } =
+          studentFindRes.docs[0]
+
+        console.log('matric no', matricNo, resetPasswordToken)
+
+        if (!resetPasswordToken || !resetPasswordExpiration) {
+          return Response.json({ message: 'Password reset not started.' }, { status: 400 })
+        }
+
+        if (isBefore(resetPasswordExpiration!, new Date())) {
+          return Response.json({ message: 'OTP Expired.' }, { status: 400 })
+        }
+
+        if (!(await bcrypt.compare(otp, resetPasswordOtpHash!))) {
+          return Response.json({ message: 'Invalid OTP.' }, { status: 400 })
+        }
+
+        // req.payload.update({
+        //   collection: 'students',
+        //   id,
+        //   data: {
+        //     resetPasswordOtpHash: null,
+        //   },
+        // })
+
+        return Response.json({
+          message: 'Success',
+          token: resetPasswordToken,
         })
       },
     },
@@ -161,6 +297,11 @@ export const Students: CollectionConfig = {
     {
       name: 'accountNo',
       type: 'text',
+    },
+    {
+      name: 'resetPasswordOtpHash',
+      type: 'text',
+      hidden: true,
     },
   ],
 }
