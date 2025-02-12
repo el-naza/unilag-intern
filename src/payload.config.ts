fe7dc3e91430b8c7bb834e66e27fe14970829da6
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2025-02-12 21:07:36 +0100

    fix(config): change email adapter to resend

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,7 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb' // database-adapter-import
 import { nodemailerAdapter } from '@payloadcms/email-nodemailer'
+import { resendAdapter } from '@payloadcms/email-resend'
 
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -16,7 +17,6 @@ import { DepartmentalCoordinators } from './collections/DepartmentalCoordinators
 import { InternshipApplications } from './collections/InternshipApplications'
 import { Employments } from './collections/Employments'
 import { Reports } from './collections/Reports'
-import { Concert_One } from 'next/font/google'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -38,17 +38,22 @@ export default buildConfig({
     Reports,
   ],
   cors: [getServerSideURL()].filter(Boolean),
-  email: nodemailerAdapter({
+  // email: nodemailerAdapter({
+  //   defaultFromAddress: process.env.EMAIL_USER!,
+  //   defaultFromName: 'UNILAG INTERNSHIP',
+  //   transportOptions: {
+  //     host: 'smtp.titan.email',
+  //     port: 465,
+  //     auth: {
+  //       user: process.env.EMAIL_USER,
+  //       pass: process.env.EMAIL_PASSWORD,
+  //     },
+  //   },
+  // }),
+  email: resendAdapter({
     defaultFromAddress: process.env.EMAIL_USER!,
     defaultFromName: 'UNILAG INTERNSHIP',
-    transportOptions: {
-      host: 'smtp.titan.email',
-      port: 465,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    },
+    apiKey: process.env.RESEND_API_KEY || '',
   }),
   plugins: [
     // storage-adapter-placeholder
