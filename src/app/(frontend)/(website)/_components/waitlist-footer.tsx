220dc64a4b4288d9da733b15201fc91cd383ea01
Kehinde Otaru <67073913+KenOtaru@users.noreply.github.com>
2025-03-22 17:44:11 +0100

    - Modifed general Company Layout to exclude waitinglist and waitinglist-signup-success pages - Created new Layouts for  waitinglist and waitinglist-signup-success pages - Updated the new Layouts accordingly and made Responsive

---
+++ src/app/(frontend)/(website)/_components/waitlist-footer.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#D2E6E4] text-primary w-full curved-top px-4 sm:px-6 xl:px-8 lg:px-24 py-12 xl:pt-24 xl:pb-16">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 xl:grid-cols-5 gap-4">
+          {/* Logo */}
+          <div className="flex justify-center xl:justify-start">
+            <Image src={'/images/unilag-logo.png'} alt="Logo" width={200} height={100} />
+          </div>
+
+          {/* Address */}
+          <div className="text-center xl:text-left">
+            <ul className="space-y-2 text-sm lg:text-[14px]">
+              <li>
+                <strong>Address:</strong> University of Lagos, University Road, Lagos Mainland,
+                Akoka, Yaba, Lagos, Nigeria.
+              </li>
+              <li>
+                Tel:{' '}
+                <a href="tel:+23498720938" className="hover:underline">
+                  +23498720938
+                </a>
+              </li>
+              <li>Response Hours: 8am to 5pm</li>
+              <li>Email: info@onlearn.com</li>
+            </ul>
+          </div>
+
+          {/* Intern Categories */}
+          <div className="text-center xl:text-left">
+            <p className="text-black font-semibold mb-3">Intern Categories</p>
+            <ul className="space-y-2 text-sm lg:text-[14px]">
+              <li>SIWES</li>
+              <li>Teaching Practice</li>
+              <li>Housemanship</li>
+              <li>Others</li>
+            </ul>
+          </div>
+
+          {/* Links */}
+          <div className="text-center xl:text-left">
+            <p className="text-black font-semibold mb-3">Connect with us</p>
+            <ul className="space-y-2 text-sm lg:text-[14px]">
+              <li className="hover:underline">
+                <Link href="/">@koonageInfotech</Link>
+              </li>
+              <li className="hover:underline">
+                <Link href="/">koonage@X</Link>
+              </li>
+              <li className="hover:underline">
+                <Link href="/">koonage@Instagram</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Newsletter */}
+          <div className="flex flex-col items-center xl:items-start">
+            <p className="text-black font-semibold mb-3 text-center xl:text-left">
+              Stay up to date with the latest information
+            </p>
+            <div className="flex w-full max-w-sm items-center space-x-2 mt-2 bg-white p-2 rounded-[14px]">
+              <Input type="email" placeholder="Email" className="border-0 outline-none" />
+              <Button type="submit" className="bg-primary text-white rounded-[12px]">
+                Send
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/app/(frontend)/(website)/_components/waitlist-navbar.tsx
@@ -0,0 +1,33 @@
+'use-client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center py-2 lg:px-24 md:px-16 sm:px-8 bg-white sticky top-0 w-full shadow-md z-20">
+      <Image src="/images/unilag-logo.png" alt="Logo" width={60} height={100} />
+
+      {/* <ul className="list-none lg:flex md:flex sm:hidden items-center lg:gap-40 md:gap-16 transition-all">
+        <li className="hover:text-[#A71C51] hover:font-semibold">
+          <Link href="/home">Home</Link>
+        </li>
+        <li className="hover:text-[#A71C51] hover:font-semibold">
+          <Link href="/company">Companies</Link>
+        </li>
+        <li className="hover:text-[#A71C51] hover:font-semibold">
+          <Link href="/blog">Blog</Link>
+        </li>
+      </ul> */}
+
+      <div className="flex gap-4 items-center mr-5 md:mr-0">
+        <Link href="/waitinglist">
+          <Button className="bg-primary text-white">Waiting List</Button>
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
