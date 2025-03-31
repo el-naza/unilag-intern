446d24c5626b72d8e8b955630626026dc9faa611
Emmanuel Ngene <ngene@koonage.com>
2025-03-31 21:08:23 +0100

    debug: attempt to fix prod

--- src/services/fetchDocs.ts
+++ src/services/fetchDocs.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { auth } from '@/auth'
 import axiosInstance from '@/utilities/axiosInstance'
 import { ValidationErrors } from '@/utilities/types'
 import { AxiosError } from 'axios'
@@ -11,12 +12,38 @@ import { stringify } from 'qs-esm'
 export default async function fetchDocs<T>(
   col: CollectionSlug,
   where: string = '',
+  token = '',
 ): Promise<{ data: T } | ValidationErrors> {
+  // console.log( 'auth' , await auth())
+  console.log(
+    '*******token getting',
+    'auth',
+    (await headers()).get('authorization'),
+    'cookie',
+    (await headers()).get('cookie'),
+    '\ntoken\n',
+    (
+      await getToken({
+        secureCookie: process.env.NODE_ENV === 'production',
+        req: { headers: await headers() },
+        secret: process.env.NEXTAUTH_SECRET,
+      })
+    )?.token!,
+  )
+
+  console.log(
+    '\ngettting',
+    await getToken({
+      secureCookie: process.env.NODE_ENV === 'production',
+      req: { headers: await headers() },
+      secret: process.env.NEXTAUTH_SECRET,
+    }),
+  )
   return (
     await axiosInstance
       .get(`/api/${col}${where}`, {
         headers: {
-          Authorization: `Bearer ${(await getToken({ secureCookie: process.env.NODE_ENV === 'production', req: { headers: await headers() }, secret: process.env.NEXTAUTH_SECRET }))?.token!}`,
+          Authorization: `Bearer ${(await getToken({ secureCookie: process.env.NODE_ENV === 'production', req: { headers: await headers() }, secret: process.env.NEXTAUTH_SECRET }))?.token! || token}`,
         },
       })
       .catch((error: AxiosError) => {
