24ba5993b757f0b73523bc92af499733345a3370
Emmanuel Ngene <ngene@koonage.com>
2025-06-18 09:34:44 +0100

    feat: add logic to handle the redirecting to other user types' login page

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,32 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 import { headers } from 'next/headers'
 
-const roleRoutesMap: Record<string, string[]> = {
-  students: ['/student'],
+const routePermissions: Record<string, { roles: string[]; loginPath: string }> = {
+  '/student': {
+    roles: ['students'],
+    loginPath: '/auth/login',
+  },
+  '/company-pages': {
+    roles: ['companies'],
+    loginPath: '/company-auth/login',
+  },
+  '/admin': {
+    roles: ['admin'],
+    loginPath: '/admin/auth',
+  },
 }
-
-export default NextAuth(authConfig).auth
 
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
+  console.log('Middleware triggered for path:', pathname)
+
+  // Skip middleware for admin auth path
+  if (pathname.startsWith('/admin/auth')) {
+    return NextResponse.next()
+  }
+
+  // Extract first segment of the path
+  const firstSegment = '/' + pathname.split('/')[1]
 
   const authUser: any = await getToken({
     secureCookie: process.env.NEXT_PUBLIC_SERVER_URL.startsWith('https'),
@@ -23,26 +41,31 @@ export async function middleware(request: NextRequest) {
   const isAuthenticated = !!authToken
 
   if (!isAuthenticated) {
+    const routeConfig = routePermissions[firstSegment]
+    const loginPath = routeConfig?.loginPath || '/auth/login'
+
+    console.log(`User is not authenticated, redirecting to: ${loginPath}`)
+
     return NextResponse.redirect(
-      new URL(`/auth/login?redirect=${request.nextUrl.pathname}`, request.url),
+      new URL(`${loginPath}?redirect=${request.nextUrl.pathname}`, request.url),
     )
   }
 
   const authRole = authUser?.user.collection
+  const routeConfig = routePermissions[firstSegment]
 
-  // Check if the user's role is allowed to access the current path
-  const allowedPaths = roleRoutesMap[authRole] || []
-  const isAllowed = allowedPaths.some((prefix) => pathname.startsWith(prefix))
-
-  if (!isAllowed) {
-    return NextResponse.redirect(new URL('/auth/login', request.url))
+  if (!routeConfig || !routeConfig.roles.includes(authRole)) {
+    const loginPath = routeConfig?.loginPath || '/auth/login'
+    console.log(
+      `User with role ${authRole} is not allowed to access ${pathname}, redirecting to:`,
+      loginPath,
+    )
+    return NextResponse.redirect(new URL(loginPath, request.url))
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  // matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-  matcher: ['/student/:path*'],
+  matcher: ['/student/:path*', '/company-pages/:path*', '/admin/:path*'],
 }
